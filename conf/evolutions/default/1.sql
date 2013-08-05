814e204c616270895ebd81ae3e7028261e5bdd05
Jinhyuk Lee <eces@mstock.org>
2013-08-06 03:47:06 +0900

    Update scheme

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,36 +1,37 @@
 # --- !Ups
 CREATE TABLE `edges` (
-    `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `ownerId` bigint(20) NOT NULL,
-    `sId` bigint(20) NOT NULL,
+    `no` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `owner` bigint(20) NOT NULL,
+    `s` bigint(20) NOT NULL,
     `sType` bigint(20) NOT NULL,
     `v` varchar(200) NOT NULL,
-    `oId` bigint(20) NOT NULL,
+    `o` bigint(20) NOT NULL,
     `oType` bigint(20) NOT NULL,
-    `data` VARCHAR(10240) NOT NULL,
-    `createdAt` DATETIME(6) NOT NULL,
-    `updatedAt` DATETIME(6) NOT NULL
+    `json` VARCHAR(10240) NOT NULL,
+    `created` DATETIME(6) NOT NULL,
+    `updated` DATETIME(6) NOT NULL
 );
 ALTER TABLE `edges` ADD INDEX ( `ownerId` );
 
-CREATE TABLE `points` (
-    `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `ownerId` bigint(20) NOT NULL,
-    `identifier` varchar(1000) NOT NULL,
-    `typeId` bigint(20) NOT NULL,
-    `createdAt` DATETIME(6) NOT NULL,
-    `updatedAt` DATETIME(6) NOT NULL,
-    `referencedAt` DATETIME(6) NOT NULL,
-    `data` varchar(10240) NOT NULL
+CREATE TABLE `nodes` (
+    `no` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `owner` bigint(20) NOT NULL,
+    `id` varchar(1000) NOT NULL,
+    `type` bigint(20) NOT NULL,
+    `created` DATETIME(6) NOT NULL,
+    `updated` DATETIME(6) NOT NULL,
+    `referenced` DATETIME(6) NOT NULL,
+    `json` varchar(10240) NOT NULL
 );
-ALTER TABLE `points` ADD INDEX ( `ownerId` );
+ALTER TABLE `nodes` ADD INDEX ( `ownerId` );
 
-CREATE TABLE `pointTypes` (
-  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `types` (
+  `no` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(200) NOT NULL UNIQUE
 );
 
 # --- !Downs
 DROP TABLE IF EXISTS `edges`;
 DROP TABLE IF EXISTS `points`;
+DROP TABLE IF EXISTS `nodes`;
 DROP TABLE IF EXISTS `pointTypes`;
