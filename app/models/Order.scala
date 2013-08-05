e6fb3f931bed38ef16a36e04da48b99a84189fc2
Jinhyuk Lee <eces@mstock.org>
2013-08-06 07:17:27 +0900

    Add order model

---
+++ app/models/Order.scala
@@ -0,0 +1,60 @@
+package models
+
+import play.api.mvc._
+import play.api.Play.current
+import play.api.db._
+import anorm._ 
+import anorm.SqlParser._
+import play.api.libs.json._
+import play.api.libs.json.Json._
+import play.api.i18n.Messages
+import play.api.Play.current
+import play.api.Logger
+import java.util.Date
+
+case class Order(var no: Long, title: String,
+  var createdAt: Date, var updatedAt: Date, var referencedAt: Date) {
+
+  def toJson: JsObject = {
+    Json.obj(
+      "$no"         -> this.no,
+      "title"       -> this.title,
+      "$createdAt"  -> this.createdAt,
+      "$updatedAt"  -> this.updatedAt,
+      "$referencedAt" -> this.referencedAt
+    )
+  }
+
+  def save: Option[Long] = {
+    val n = Node(Json.obj( "order" -> this.toJson ))
+    // app@mintpresso.com
+    n.ownerNo = 1
+    n.save
+  }
+
+  def delete: Boolean = {
+    Node.delete(this.no)
+  }
+
+  def prepare = {
+    // load plan
+
+    // match procedure
+
+    // send scheduled message on Akka to OrderActor.
+
+    // change running state of order.
+
+    Logger.info("Not implemented")
+    None
+  }
+
+  def cancel = {
+    // send stop command to Actor.
+    
+    // change running state to paused.
+
+    Logger.info("Not implemented")
+    None
+  }
+}
