1fa62f802029af28e5d947909fcafe6c02617c7b
Jinhyuk Lee <eces@mstock.org>
2013-08-06 03:48:10 +0900

    Add basic test spec for node manipulation

---
+++ test/NodeSpec.scala
@@ -0,0 +1,52 @@
+package test
+
+import org.specs2.mutable._
+import org.specs2.matcher.MatchResult
+
+import play.api.test._
+import play.api.test.Helpers._
+import play.api._
+import play.api.mvc._
+import play.api.i18n.Messages
+import play.api.data.Forms._
+import play.api.data._
+import play.api.Play.current
+import play.api.libs._
+import play.api.libs.ws._
+import play.api.cache._
+import play.api.libs.json._
+import play.api.libs.iteratee._
+import scala.concurrent.stm._
+import scala.concurrent._
+import scala.concurrent.duration._
+
+import java.util.Date
+
+class NodeSpec extends Specification {
+
+  "Node" can {
+
+    "save and return no" in {
+      true
+    }
+
+    "find one by no" in {
+      true
+    }
+
+    "find one by id" in {
+      true
+    }
+
+
+  }
+
+  "Node API" should {
+    "create new type as needed" in {
+      true
+    }
+    "not create new type if it's already exist" in {
+      true
+    }
+  }
+}
