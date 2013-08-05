d5b94597da5b96769bab501e3a88442f890ba2ff
Jinhyuk Lee <eces@mstock.org>
2013-08-06 03:47:40 +0900

    Add key model

--- app/models/Key.scala
+++ app/models/Key.scala
@@ -11,9 +11,32 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import java.util.Date
 
-case class Key(var no: Long, var ownerNo: Long, var url: List[String], var address: List[String])
+case class Key(var id: String, var no: Long, var ownerNo: Long, var url: List[String], 
+	var address: List[String], var createdAt: Date) {
+
+  def toJson: JsObject = {
+    Json.obj(
+      "$id" ->        this.id,
+      "$no" ->        this.no,
+      "ownerNo" ->    this.ownerNo,
+      "url" ->        this.url,
+      "address" ->    this.address
+    )
+  }
+}
 
 object Key {
+  def apply(json: JsValue): Key = {
+    val key = (json \ "key").as[JsObject]
+    Key(
+      (key \ "$id").as[String],
+      (key \ "$no").as[Long],
+      (key \ "ownerNo").as[Long],
+      (key \ "url").as[List[String]],
+      (key \ "address").as[List[String]],
+      (key \ "$createdAt").as[Date]
+    )
+  }
   def findByNo(no: Long): Option[Key] = {
     Logger.info("Not implemented")
     None
