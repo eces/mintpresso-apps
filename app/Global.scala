757200c7e4fc4316192ee1b472527ed3adc5f91f
Jinhyuk Lee <eces@mstock.org>
2013-08-06 23:35:05 +0900

    Add API Filter

--- app/Global.scala
+++ app/Global.scala
@@ -5,6 +5,39 @@ import play.api.libs.concurrent._
 import play.api.Play.current
 
 object Global extends GlobalSettings {
+  
+  val APIFilter = Filter { (next, rh) =>
+    val d = new java.util.Date
+    rh.path match {
+      case p if p == "/" => next(rh)
+      case p if p.endsWith(".json") => {
+        next(rh).withHeaders (
+          "Content-Type" -> "application/json; charset=utf-8",
+          "Cache-Control" -> "max-age=86400",
+          "X-Content-Type-Options" -> "nosniff"
+        )
+      }
+      case p if p.endsWith(".xml") => {
+        next(rh).withHeaders (
+          "Content-Type" -> "application/xml; charset=utf-8",
+          "Cache-Control" -> "max-age=86400",
+          "X-Content-Type-Options" -> "nosniff"
+        )
+      }
+      case _ => {
+        next(rh).withHeaders (
+          "Date" -> d.getTime.toString,
+          "Content-Type" -> "application/json; charset=utf-8",
+          "Cache-Control" -> "max-age=0, private, must-revalidate",
+          "X-Content-Type-Options" -> "nosniff"
+        )
+      }
+    }
+  }
+
+  override def doFilter(action: EssentialAction) = {
+    APIFilter(action)
+  }
 
   override def onStart(app: Application) {
 
