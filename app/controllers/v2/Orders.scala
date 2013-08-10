6227a605c52e0aa2e7c2429e170d9b271b4ef6b4
Jinhyuk Lee <eces@mstock.org>
2013-08-10 19:08:53 +0900

    Support prepare, cancel in Order, Pickup API

--- app/controllers/v2/Orders.scala
+++ app/controllers/v2/Orders.scala
@@ -2,14 +2,37 @@ package controllers.v2
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.json.Json._
 import controllers._
+import models.{Node, Order}
 
 object Orders extends Controller with Secured {
 
-  def findOne(no: Long) = TODO
+  def prepare(no: Long) = Signed("manage_order") { implicit request => implicit user =>
+    Node.findOneByNo(no) match {
+      case Some(node) => 
+        if(Order(node.toTypedJson).prepare){
+          Accepted
+        }else{
+          Ok
+        }
+      case None =>
+        NotFound
+    }
+  }
 
-  def prepare(no: Long) = TODO
-
-  def cancel(no: Long) = TODO
+  def cancel(no: Long) = Signed("manage_order") { implicit request => implicit user =>
+    Node.findOneByNo(no) match {
+      case Some(node) => 
+        if(Order(node.toTypedJson).cancel){
+          Accepted
+        }else{
+          Ok
+        }
+      case None =>
+        NotFound
+    }
+  }
 
 }
--- app/controllers/v2/Pickups.scala
+++ app/controllers/v2/Pickups.scala
@@ -2,17 +2,74 @@ package controllers.v2
 
 import play.api._
 import play.api.mvc._
+import play.api.cache._
+import play.api.libs.json._
+import play.api.libs.json.Json._
+import play.api.Play.current
 import controllers._
+import models.{Node, Edge, Type, Pickup, User}
 
 object Pickups extends Controller with Secured {
 
   // request.path.endsWith json or xml
-  def findOneById(id: String) = TODO
+  def findOneById(id: String) = Action { implicit request =>
+    Node.findOneByTypeNoAndId(Type("pickup").no, id)(User.Default) match {
+      case Some(node) => 
+        val pickup = Pickup(node.toTypedJson)
+        Cache.getAs[String](s"pickup ${pickup.no} json") match {
+          case Some(json) => Callback(Results.Ok, Json.parse(json))
+          case None => Status(429)("pickup.cache.empty")
+        }
+      case None =>
+        NotFound
+    }
+  }
 
-  def findOneByNo(no: Long) = TODO
+  def findOneByNo(no: Long) = Signed("read_pickup") { implicit request => implicit user =>
+    Node.findOneByNo(no) match {
+      case Some(node) => 
+        val pickup = Pickup(node.toTypedJson)
+        Edge.findOne(user.no, "place", pickup.no) match {
+          case Some(e) => {
+            // order 20 callback pickup 
+            Cache.getAs[String](s"pickup ${pickup.no} json") match {
+              case Some(json) => Callback(Results.Ok, Json.parse(json))
+              case None => Status(429)("pickup.cache.empty")
+            }
+          }
+          case None => {
+            Forbidden
+          }
+        }
+      case None =>
+        NotFound
+    }
+  }
 
-  def prepare(no: Long) = TODO
+  def prepare(no: Long) = Signed("manage_pickup") { implicit request => implicit user =>
+    Node.findOneByNo(no) match {
+      case Some(node) => 
+        if(Pickup(node.toTypedJson).prepare){
+          Accepted
+        }else{
+          Ok
+        }
+      case None =>
+        NotFound
+    }
+  }
 
-  def cancel(no: Long) = TODO
+  def cancel(no: Long) = Signed("manage_pickup") { implicit request => implicit user =>
+    Node.findOneByNo(no) match {
+      case Some(node) => 
+        if(Pickup(node.toTypedJson).cancel){
+          Accepted
+        }else{
+          Ok
+        }
+      case None =>
+        NotFound
+    }
+  }
 
 }
