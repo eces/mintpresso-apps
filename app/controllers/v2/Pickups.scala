abf5a15fb95ecfedc5b31976924ccf5f80d358ac
Jinhyuk Lee <eces@mstock.org>
2013-08-13 21:32:10 +0900

    Get result from order

--- app/controllers/v2/Pickups.scala
+++ app/controllers/v2/Pickups.scala
@@ -33,18 +33,15 @@ object Pickups extends Controller with Secured {
   def findOneByNo(no: Long) = Signed("read_pickup") { implicit request => implicit user =>
     Node.findOneByNo(no) match {
       case Some(node) => 
-        val pickup = Pickup(node.toTypedJson)
-        Edge.findOne(user.no, "place", pickup.no) match {
-          case Some(e) => {
-            // order 20 callback pickup 
-            Cache.getAs[String](s"pickup ${pickup.no} json") match {
+        Edge.findAllByTypeAndObject("order", "place", node) match {
+          case first :: other => {
+            val sNo = first.s.no
+            Cache.getAs[String](s"order ${sNo} json") match {
               case Some(json) => Callback(Results.Ok, Json.parse(json))
-              case None => Status(429)("pickup.cache.empty")
+              case None => Status(429)("pickup.cache.empty") 
             }
           }
-          case None => {
-            Forbidden
-          }
+          case List() => Status(404)("order.empty") 
         }
       case None =>
         NotFound
